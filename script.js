3b6bb519eadb85db1792366842b5409eafbc378c
Rishav Das <rishavdas27602@gmail.com>
2025-01-31 21:53:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	   event.preventDefault(); // Prevent form submission
 
+    // Get the form element
+    let form = document.getElementById("form1");
+
+    // Retrieve the values of 'fname' and 'lname' input fields
+    let firstName = form.elements["fname"].value;
+    let lastName = form.elements["lname"].value;
+
+    // Display the full name in an alert
+    alert(firstName + " " + lastName);
 }
